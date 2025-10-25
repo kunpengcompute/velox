9a4e04d2eb92e064812769589f2973dc65e3b261
gcw_yU2WmohA <chenyixin24@h-partners.com>
2025-10-25 18:41:02 +0800

    update: 更新文件 RleBpDecoder.cpp
    Signed-off-by: gcw_yU2WmohA <chenyixin24@h-partners.com>

--- velox/dwio/parquet/reader/RleBpDecoder.cpp
+++ velox/dwio/parquet/reader/RleBpDecoder.cpp
@@ -66,32 +66,12 @@ void RleBpDecoder::readBits(
       bits::fillBits(
           outputBuffer, numWritten, numWritten + consumed, value_ != 0);
     } else {
-#if defined(__ARM_FEATURE_SVE) && defined(__aarch64__)
-      if (bitOffset_ % 8 == 0 && numWritten % 8 == 0 && 
-          (reinterpret_cast<uintptr_t>(bufferStart_) % 8 == 0)) {
-        bits::copyBitsSVE(
-            reinterpret_cast<const uint64_t*>(bufferStart_),
-            static_cast<uint64_t>(bitOffset_),
-            outputBuffer,
-            static_cast<uint64_t>(numWritten),
-            static_cast<uint64_t>(consumed));
-      } else {
-        bits::copyBits(
-            reinterpret_cast<const uint64_t*>(bufferStart_),
-            static_cast<uint64_t>(bitOffset_),
-            outputBuffer,
-            static_cast<uint64_t>(numWritten),
-            static_cast<uint64_t>(consumed));
-      }
-#else
       bits::copyBits(
           reinterpret_cast<const uint64_t*>(bufferStart_),
-          static_cast<uint64_t>(bitOffset_),
+          bitOffset_,
           outputBuffer,
-          static_cast<uint64_t>(numWritten),
-          static_cast<uint64_t>(consumed));
-#endif
-
+          numWritten,
+          consumed);
       int64_t offset = bitOffset_ + consumed;
       bufferStart_ += offset >> 3;
       bitOffset_ = offset & 7;
