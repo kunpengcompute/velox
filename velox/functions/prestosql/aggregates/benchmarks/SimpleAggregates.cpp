2e9ad9d749a452b41f00bab7e482a1d2a8cee750
oldli883 <872049064@qq.com>
2025-12-24 10:01:31 +0800

    [refactor]fix some bug

--- velox/functions/prestosql/aggregates/benchmarks/SimpleAggregates.cpp
+++ velox/functions/prestosql/aggregates/benchmarks/SimpleAggregates.cpp
@@ -37,7 +37,7 @@ namespace {
 class SimpleAggregatesBenchmark : public HiveConnectorTestBase {
  public:
   SimpleAggregatesBenchmark() {
-    OperatorTestBase::SetUpTestCase();
+    // OperatorTestBase::SetUpTestCase();
     HiveConnectorTestBase::SetUp();
 
     inputType_ = ROW({
@@ -166,7 +166,7 @@ class SimpleAggregatesBenchmark : public HiveConnectorTestBase {
         std::move(plan),
         0,
         core::QueryCtx::create(executor_.get()),
-        exec::Task::ExecutionMode::kParallel);
+        exec::Task::ExecutionMode::kSerial);
   }
 
  private:
